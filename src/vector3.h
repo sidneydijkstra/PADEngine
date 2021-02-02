d3edfa6895b747d15b7b63f42d6d62a635eaa3f3
shcalbers <34623185+shcalbers@users.noreply.github.com>
2021-02-02 13:14:01 +0100

    Add doxygen documentation for vector3.h

--- src/vector3.h
+++ src/vector3.h
@@ -1,3 +1,9 @@
+/// @file vector3.h
+/// @brief Standalone header providing Vector3 functionality.
+/// @author Sander Albers, Sidney Dijkstra, Naomi Poort
+/// @version 1.0.0
+/// @date 30/01/2021
+
 #ifndef PAD_ENGINE_VECTOR3_HEADER
 #define PAD_ENGINE_VECTOR3_HEADER
 
@@ -11,26 +17,56 @@ namespace pad {
 
 using real = float;
 
+/// @brief Struct describing a 3-dimensional vector.
 struct Vector3 {
 
     real x, y, z;
 
+    /// @brief Creates a Vector3 pointing to the origin.
+    /// @return A Vector3 object initialized as {0, 0, 0}.
     static Vector3 zero() noexcept;
-
+    
+    /// @brief Creates a Vector3 pointing forward.
+    /// @return A Vector3 object initialized as {0, 0, 1}.
     static Vector3 forward() noexcept;
+    
+    /// @brief Creates a Vector3 pointing back.
+    /// @return A Vector3 object initialized as {0, 0, -1}.
     static Vector3 back() noexcept;
 
+    /// @brief Creates a Vector3 pointing up.
+    /// @return A Vector3 object initialized as {0, 1, 0}.
     static Vector3 up() noexcept;
+
+    /// @brief Creates a Vector3 pointing down.
+    /// @return A Vector3 object initialized as {0, -1, 0}.
     static Vector3 down() noexcept;
 
+    /// @brief Creates a Vector3 pointing to the right.
+    /// @return A Vector3 object initialized as {1, 0, 0}.
     static Vector3 right() noexcept;
+
+    /// @brief Creates a Vector3 pointing to the left.
+    /// @return A Vector3 object initialized as {-1, 0, 0}.
     static Vector3 left() noexcept;
 
+    /// @brief Creates a Vector3 pointing towards positive infinity.
+    /// @return A Vector3 object initialized as {INF, INF, INF}.
     static Vector3 positiveInfinity() noexcept;
+
+    /// @brief Creates a Vector3 pointing towards negative infinity.
+    /// @return A Vector3 object initialized as {-INF, -INF, -INF}.
     static Vector3 negativeInfinity() noexcept;
 
+    /// @brief Creates a Vector3 pointing in a random direction, with a magnitude of 1.
+    /// @return A Vector3 object with random coordinates in the range [-1, +1] with a magnitude of 1.
     static Vector3 random() noexcept;
 
+    /// @brief Constructs a new Vector3 object with the given coordinates.
+    /// @param x The value of the x-coordinate.
+    /// @param y The value of the y-coordinate.
+    /// @param z The value of the z-coordinate.
+    /// @return A Vector3 object with the given coordinates.
     Vector3(real x, real y, real z) noexcept;
 
     Vector3() noexcept = default;
@@ -40,50 +76,183 @@ struct Vector3 {
     Vector3& operator=(const Vector3&) noexcept = default;
     Vector3& operator=(Vector3&&) noexcept = default;
 
+    /// @brief Gets the element at position 'n' of the Vector3 object.
+    /// @param n The index of the element to retrieve, must be either 0 (for x), 1 (for y), or 2 (for z).
+    /// @return A const reference to the x-coordinate if n equals 0, the y-coordinate if n equals 1 or the z-coordinate if n equals 2.
     const real& operator[](int n) const noexcept;
+
+    /// @brief Gets the element at position 'n' of the Vector3 object.
+    /// @param n The index of the element to retrieve, must be either 0 (for x), 1 (for y), or 2 (for z).
+    /// @return A reference to the x-coordinate if n equals 0, the y-coordinate if n equals 1 or the z-coordinate if n equals 2.
     real& operator[](int n) noexcept;
 
+    /// @brief Adds another Vector3 to this Vector3 object.
+    /// @param other The vector to add to this vector.
+    /// @return A reference to the Vector3 object that was added to.
     Vector3& operator+=(const Vector3& other) noexcept;
+
+    /// @brief Subtracts another Vector3 from this Vector3 object.
+    /// @param other The vector to subtract from this vector.
+    /// @return A reference to the Vector3 object that was subtracted from.
     Vector3& operator-=(const Vector3& other) noexcept;
 
+    /// @brief Multiplies this Vector3 object by the given number.
+    /// @param scalar The amount by which to multiply this vector.
+    /// @return A reference to the Vector3 object that was multiplied.
     Vector3& operator*=(real scalar) noexcept;
+
+    /// @brief Divides this Vector3 object by the given number.
+    /// @param scalar The amount by which to divide this vector.
+    /// @return A reference to the Vector3 object that was divided.
     Vector3& operator/=(real scalar) noexcept;
 
+    /// @brief Converts this Vector3 object to a glm::vec3 object.
+    /// @return This vector as a glm::vec3 object.
     operator glm::vec3() const noexcept;
+
+    /// @brief Converts this Vector3 object to a glm::vec3 object.
+    /// @return This vector as a glm::vec3 object.
     glm::vec3 glm();
 
+    /// @brief Gets the magnitude of the Vector3 object.
+    /// @return The magnitude, also known as the length, of the vector.
     real magnitude() const noexcept;
+
+    /// @brief Gets the squared magnitude of the Vector3 object.
+    ///
+    /// Almost the same as <tt>Vector3::magnitude</tt> except the more expensive square root operation is not applied;
+    /// thus giving a value that is 'n' times as large as the magnitude, where 'n' equals the magnitude itself.
+    /// 
+    /// This method may be used where the actual value of the magnitude doesn't really matter, but where you do still need
+    /// to compare the length(s) of one or more vectors; or when you need the squared magnitude for a calculation. As this
+    /// method avoids taking the square root, which is an expensive operation, using this method might improve performance.
+    /// 
+    /// @return The squared magnitude of the vector.
     real magnitudeSqr() const noexcept;
 
     ~Vector3() noexcept = default;
 
 };
 
+/// @brief Compares two vector to each other for equality.
+/// @param left_vector The Vector3 object to the left of the equality operator.
+/// @param right_vector The Vector3 object to the right of the equality operator.
+/// @return True if both vectors are (roughly) equal, false otherwise.
 bool operator==(const Vector3& left_vector, const Vector3& right_vector) noexcept;
+
+/// @brief Compares two vector to each other inequality.
+/// @param left_vector The Vector3 object to the left of the inequality operator.
+/// @param right_vector The Vector3 object to the right of the inequality operator.
+/// @return True if both vectors are (roughly) inequal, false otherwise.
 bool operator!=(const Vector3& left_vector, const Vector3& right_vector) noexcept;
 
+/// @brief Unary plus operator, essentially the same as '<tt>Vector3::zero()</tt> + vector'.
+///
+/// This operator has no effect on its input and exists mostly as an antagonist to
+/// the unary minus operator, which has a more interesting effect on its input.
+/// 
+/// @param vector The Vector3 object this operator was applied to.
+/// @return A Vector3 object equal to the input.
 Vector3 operator+(Vector3 vector) noexcept;
+
+/// @brief Unary minus operator, essentially the same as '<tt>Vector3::zero()</tt> - vector'.
+///
+/// As this operator is the same as '<tt>Vector3::zero()</tt> - vector', the output will be
+/// a vector of the same length of the input, but pointing in the opposite direction.
+/// 
+/// @param vector The Vector3 object this operator was applied to.
+/// @return A Vector3 object equal to the inverse of the input.
 Vector3 operator-(Vector3 vector) noexcept;
 
+/// @brief Adds two vectors to each other.
+/// @param left_vector The Vector3 object to the left of the addition operator.
+/// @param right_vector The Vector3 object to the right of the addition operator.
+/// @return A Vector3 object equal to the sum of the given vectors.
 Vector3 operator+(Vector3 left_vector, const Vector3& right_vector) noexcept;
+
+/// @brief Subtracts two vectors from each other.
+/// @param left_vector The Vector3 object to the left of the subtraction operator.
+/// @param right_vector The Vector3 object to the right of the subtraction operator.
+/// @return A Vector3 object equal to the difference of the given vectors.
 Vector3 operator-(Vector3 left_vector, const Vector3& right_vector) noexcept;
 
+/// @brief Multiplies a vector by a scalar.
+/// @param vector The Vector3 object to the left of the multiplication operator.
+/// @param scalar The <tt>real</tt> number to the right of the multiplication operator.
+/// @return A Vector3 object equal to <p>vector</p> multiplied by <p>scalar</p>.
 Vector3 operator*(Vector3 vector, real scalar) noexcept;
+
+/// @brief Multiplies a vector by a scalar.
+/// @param scalar The <tt>real</tt> number to the left of the multiplication operator.
+/// @param vector The Vector3 object to the right of the multiplication operator.
+/// @return A Vector3 object equal to <p>vector</p> multiplied by <p>scalar</p>.
 Vector3 operator*(real scalar, Vector3 vector) noexcept;
+
+/// @brief Divides a vector by a scalar.
+/// @param vector The Vector3 object to the left of the division operator.
+/// @param scalar The <tt>real</tt> number to the right of the division operator.
+/// @return A Vector3 object equal to <p>vector</p> divided by <p>scalar</p>.
 Vector3 operator/(Vector3 vector, real scalar) noexcept;
 
+/// @brief Gets the absolute value of a vector, same as <tt>Vector3::magnitude()</tt>
+/// @param vector The Vector3 object to get the absolute value of.
+/// @return The absolute value (also length or magnitude) of the given vector.
 real abs(const Vector3& vector) noexcept;
 
+/// @brief Divides a vector by its magnitude.
+/// @param vector The vector to normalize.
+/// @return The normalized vector of the given vector.
 Vector3 normalize(Vector3 vector) noexcept;
+
+/// @brief Limits the length of a given vector by <p>max_magnitude</p>
+/// @param vector The vector to limit the length of.
+/// @param max_magnitude The magnitude to limit the length of <p>vector</p> to.
+/// @return A vector that is at most <p>max_magnitude</p> in length, pointing in the same direction as <p>vector</p>
 Vector3 limit(Vector3 vector, real max_magnitude) noexcept;
 
+/// @brief Linearly interpolates between the given vectors.
+///
+/// Essentially moves the first vector towards the second, by a percentage given by the parameter <p>factor</p>.
+/// 
+/// @param start_vector The vector to move from.
+/// @param end_vector The vector to move towards.
+/// @param factor A percentage given as floating point number in the range 0.0 (0%) to 1.0 (100%).
+/// @return A vector equal to <tt>start_vector + factor*(end_vector-start_vector)</tt>
 Vector3 lerp(const Vector3& start_vector, const Vector3& end_vector, real factor) noexcept;
+
+/// @brief Moves the first vector towards the second, moving at most <p>max_delta</p>.
+///
+/// Almost the same as <tt>lerp</tt>, except instead of moving by a percentage, you move by fixed number until
+/// you've reached the </p>end_vector<p>. 
+/// 
+/// @param start_vector The vector to move from.
+/// @param end_vector The vector to move towards.
+/// @param max_delta The maximum distance that may be mvoed from <p>start_vector</p> to <p>end_vector</p>.
+/// @return A vector equal to <tt>start_vector + factor*(end_vector-start_vector)</tt>
 Vector3 moveTowards(const Vector3& start_vector, const Vector3& end_vector, real max_delta) noexcept;
 
+/// @brief Calculates the angle between two given vectors.
+/// @param left_vector One of the vectors to calculate the angle between.
+/// @param right_vector One of the vectors to calculate the angle between. 
+/// @return The angle between the two given vectors in radians.
 real angle(const Vector3& left_vector, const Vector3& right_vector) noexcept;
+
+/// @brief Calculates the distance between two given vectors.
+/// @param start_vector The start point.
+/// @param end_vector The end point.
+/// @return The distance between <p>start_vector</p> and <p>end_vector</p>.
 real distance(const Vector3& start_vector, const Vector3& end_vector) noexcept;
 
+/// @brief Calculates the dot product of the given vectors.
+/// @param left_vector The vector that would appear to the left of the dot operator.
+/// @param right_vector The vector that would appear to the right of the dot operator.
+/// @return The dot product of the given vectors.
 real dot(const Vector3& left_vector, const Vector3& right_vector) noexcept;
+
+/// @brief Calculates the cross product of the given vectors.
+/// @param left_vector The vector that would appear to the left of the cross operator.
+/// @param right_vector The vector that would appear to the right of the cross operator.
+/// @return The cross product of the given vectors.
 Vector3 cross(const Vector3& left_vector, const Vector3& right_vector) noexcept;
 
 /* * * * * * * * *
